91442c06f64051be29065535eda1173935073613
maniofisint <maniofisint@gmail.com>
2021-01-23 16:17:36 +0330

    ..

--- learn/AI/tree2.py
+++ learn/AI/tree2.py
@@ -12,20 +12,22 @@ class tree:
 
     def __init__(self):
         self.root = None
-        self.graph = pydot.Dot(graph_type='digraph', strict=True)
+        self.graph = pydot.Dot(graph_type='graph', strict=True)
         self.size = 0
 
     def _make_tree(self, input, prnt=None):
         if not isinstance(input, tuple):
-            u = pydot.Node(name=str(self.size), label=str(input), style='filled', fillcolor='green')
+            u = pydot.Node(name=str(self.size), label=str(input), shape='plain')
+            self.graph.add_node(u)
             self.size += 1
             self.graph.add_node(u)
             new_node = self.Node(parent=prnt, is_leaf=True, element=input)
             return new_node, u
 
         parent = self.Node(parent=prnt)
-        string = 'Node'
-        u = pydot.Node(name=str(self.size), label=string, style="filled", fillcolor="green")
+        string = 'Node' 
+        u = pydot.Node(name=str(self.size), label=' ', shape='point')
+        self.graph.add_node(u)
         self.size += 1
         for i in input:
             node, v = self._make_tree(i, parent)
@@ -41,11 +43,34 @@ class tree:
     def write_graph(self, name='tree2_output'):
         self.graph.write_png(name+'.png')
 
+    def to_string(self, p:Node):
+        if p.is_leaf:
+            return str(p.element)
+        out = '('
+        for i in p.children:
+            out += self.to_string(i) + ','
+        
+        return out[:-1] + ')'
+
+    def iteration(self, address:tuple):
+        p = self.root
+        for i in address:
+            p = p.children[i]
+        return p
+
+    def reference(self, ref):
+        p = self.iteration(ref)
+        return self.to_string(p)
+
 
 
 
 
 t = tree()
-tup = (7, (5, 4), 6)
+tup = (7, (5, 4), 6, (2,(3,4,5), 5, 6))
+tup = (((1, 2), 3), (4, (5, 6)), 7, (8, 9, 10))
 t.make_tree(tup)
-t.write_graph()
+t.write_graph('hello')
+Tup = (1,1,1)
+Tup = (0,)
+print(t.reference(Tup))
